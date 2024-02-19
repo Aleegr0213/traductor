41d076cfcfee81720e6a6de0697c44986980920f
Aleegr0213 <aleegr0213@gmail.com>
2024-02-19 15:07:47 -0600

    Genera un csv general

--- hands_model.py
+++ hands_model.py
@@ -5,6 +5,7 @@ import pandas as pd
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+landmarks_direction_list = []
 
 with mp_hands.Hands(
     static_image_mode=False,
@@ -21,23 +22,15 @@ with mp_hands.Hands(
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         results = hands.process(frame_rgb)
-
         if results.multi_hand_landmarks is not None:
+            landmarks_direction_final = {}
             for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    frame, hand_landmarks, mp_hands.HAND_CONNECTIONS
-                )
-                landmarks_direction = {}
+                mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
                 for id, landmark in enumerate(hand_landmarks.landmark):
                     cx, cy = int(landmark.x * width), int(landmark.y * height)
-                    landmarks_direction[f"Point_{id}"] = (cx, cy)
+                    landmarks_direction_final[f"Point_{id}"] = (cx, cy)
+            landmarks_direction_list.append(landmarks_direction_final)
             
-                landmarks_list = [{"Point": point, "x": xy[0], "y": xy[1]} 
-                                  for point, xy in landmarks_direction.items()]
-                df = pd.DataFrame(landmarks_list)
-
-                print(df)
-
         cv2.imshow("Frame", frame)
         if cv2.waitKey(1) & 0xFF ==27:
             break
@@ -45,3 +38,14 @@ with mp_hands.Hands(
 cap.release()
 cv2.destroyAllWindows()
 
+# Convertir la lista de diccionarios en DataFrame
+landmarks_list = []
+for landmarks_direction_final in landmarks_direction_list:
+    landmarks_list.extend([{"Point": point, "x": xy[0], "y": xy[1]} 
+                           for point, xy in landmarks_direction_final.items()])
+
+df = pd.DataFrame(landmarks_list)
+
+# Guardar el DataFrame en un archivo CSV
+df.to_csv("archivo.csv", index=False)
+print(df)
